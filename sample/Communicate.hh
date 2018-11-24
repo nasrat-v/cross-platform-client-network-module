8366587e5b8882753499d38342435f0bae5113d3
Valentin Nasraty <valentin.nasraty@epitech.eu>
2018-11-24 12:21:53 +0100

    Create Communicate.hh

---
+++ sample/Communicate.hh
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "WinNetwork.hh"
+
+# define PORT				6667
+# define HOSTNAME			"chat.freenode.net"
+# define NICKNAME			"nick toto"
+# define USERNAME			"user toto  8 * :toto"
+# define END_CHAR_SRV		"\r\n"
+
+class Communicate
+{
+public:
+	Communicate();
+	~Communicate();
+
+	void		communicateWithIRCServer();
+	ERR			initIRCClient();
+
+private:
+	/* Attributes */
+	bool		_isInit;
+	WinNetwork	_network;
+
+	/* Methods */
+	const WinNetwork::t_serverParam	initConfigurationIRCServer();
+};
