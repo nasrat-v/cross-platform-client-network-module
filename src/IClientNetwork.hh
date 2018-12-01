cc073f59716dda021d541c6bf363286c123a6dc7
Valentin Nasraty <valentin.nasraty@epitech.eu>
2018-12-01 20:10:17 +0100

    Update IClientNetwork.hh

--- src/IClientNetwork.hh
+++ src/IClientNetwork.hh
@@ -1,4 +1,6 @@
-#pragma once
+
+#ifndef __ICLIENTNETWORK__
+#define __ICLIENTNETWORK__
 
 #ifdef _WIN32
 	#include <winsock2.h>
@@ -25,42 +27,43 @@ public:
 
 	typedef struct		s_serverParam
 	{
-		const char		*ipAddr; // ip address of server, if NULL set hostname - ex: 127.0.0.1 - can be NULL if hostName != NULL - priority if both are set
-		const char		*hostName; // hostname of server only if ip address is unknown - ex: chat.freenode.net - can be NULL if ipAddr != NULL
-		const char		*serviceName; // only if hostname is set - ex: http - can be NULL
-		short			port; // port of server - ex: 6667 - can't be NULL
-		int				ipType; // ip type used by server - ex:: AF_INET for IPV4 or AF_INET6 for IPV6 - can't be NULL
-		int				socketType; // socket type used by server - ex: SOCK_STREAM - can't be NULL
-		int				protocol; // ip protocol used by server - ex: IPPROTO_TCP for TCP protocol - can't be NULL
-		const char		*endCharSrv; // end character when sending msg - ex: \r\n for IRC - can be NULL
-	}					t_serverParam;
+		const char	*ipAddr; // ip address of server, if NULL set hostname - ex: 127.0.0.1 - can be NULL if hostName != NULL - priority if both are set
+		const char	*hostName; // hostname of server only if ip address is unknown - ex: chat.freenode.net - can be NULL if ipAddr != NULL
+		const char	*serviceName; // only if hostname is set - ex: http - can be NULL
+		short		port; // port of server - ex: 6667 - can't be NULL
+		int		ipType; // ip type used by server - ex:: AF_INET for IPV4 or AF_INET6 for IPV6 - can't be NULL
+		int		socketType; // socket type used by server - ex: SOCK_STREAM - can't be NULL
+		int		protocol; // ip protocol used by server - ex: IPPROTO_TCP for TCP protocol - can't be NULL
+		const char	*endCharSrv; // end character when sending msg - ex: \r\n for IRC - can be NULL
+	}			t_serverParam;
 
-	virtual ERR					initNetworkClient(const t_serverParam &srvParam) = 0;
-	virtual ERR					connectToServer() = 0;
-	virtual void				deconnectToServer() = 0;
-	virtual ERR					readData(std::string &data) = 0;
-	virtual ERR					writeData(const std::string &data) = 0;
-	virtual bool				isDataToRead() = 0;
+	virtual ERR		initNetworkClient(const t_serverParam &srvParam) = 0;
+	virtual ERR		connectToServer() = 0;
+	virtual void		deconnectToServer() = 0;
+	virtual ERR		readData(std::string &data) = 0;
+	virtual ERR		writeData(const std::string &data) = 0;
+	virtual bool		isDataToRead() = 0;
 
 protected:
 	/* Attributes */
 #ifdef _WIN32
-	SOCKET						_sock;
+	SOCKET				_sock;
 	std::vector<SOCKADDR_IN>	_sins;
-	WSADATA						_wsaData;
+	WSADATA				_wsaData;
 #elif __linux__
 	// Implements linux attributes
 #endif
 
-	fd_set						_fd_set;
-	struct addrinfo				*_addrInfo;
-	t_serverParam				_srvParam;
+	fd_set				_fd_set;
+	struct addrinfo			*_addrInfo;
+	t_serverParam			_srvParam;
 
 	/* Methods */
-	virtual ERR					initSocket() = 0;
-	virtual ERR					initHandleSocketWithIpAddress() = 0;
-	virtual void				initHandleSocketWithHostname() = 0;
-	virtual ERR					findIpAddrWithHostname() = 0;
-	virtual void				clearSocket() = 0;
+	virtual ERR		initSocket() = 0;
+	virtual ERR		initHandleSocketWithIpAddress() = 0;
+	virtual void		initHandleSocketWithHostname() = 0;
+	virtual ERR		findIpAddrWithHostname() = 0;
+	virtual void		clearSocket() = 0;
 };
 
+#endif // !__ICLIENTNETWORK__
